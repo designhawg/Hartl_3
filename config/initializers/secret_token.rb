ef1b89142bf236197337bf47c1c12384ad2ed835
designhawg <designhawg@mac.com>
2012-02-24 01:26:48 -0600

    Happy to be here

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hartlsample::Application.config.secret_token = '76f021263410cb9006933914de9a3df790778451c49d535d57218caf8712f0c1e02dadb787541456c12d6cb55a4a1ae7d73cd0be1469a3644bf99772f8e9bd43'
